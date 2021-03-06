76391d2e8b5b7a5afad56bcbd0d6b6b5ede1a032
parasolja <parasolja@gmail.com>
2021-03-06 16:40:32 +0100

    Added a new button, cleaned up

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 import React from "react";
 import {Grid} from '@material-ui/core';
 import Background from './img/hedonism.jpg';
+import {Typography} from '@material-ui/core';
+import { Button } from '@material-ui/core';
 
 const sectionStyle = {
   height: "100vh",
-
+  color: 'white',
+  padding: '50px',
   backgroundImage:
   `url(${Background})`,
-
   backgroundRepeat: 'no-repeat',
   backgroundSize: 'cover',
   backgroundPosition: 'center',
@@ -20,8 +22,24 @@ const Header = () => {
         direction="column"
         justify="space-evenly"
         alignItems="center"
+        margin="50px"
     >
       <Grid item>
+        <Typography variant="h1">
+          Find the best restaurants in your area
+        </Typography>
+        <Button 
+        
+        style={{
+      
+          borderColor: '#fff',
+          color: "#fff",
+          padding: "18px 36px",
+          fontSize: "18px",
+          marginTop: '50px'}}
+        variant="outlined" >
+          Discover
+        </Button>
       </Grid>
     </Grid>
   );
