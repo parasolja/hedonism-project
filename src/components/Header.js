46d3536be53ec64aea8b7f43f29728e7d9640339
parasolja <parasolja@gmail.com>
2021-03-08 16:32:20 +0100

    Changed the main section, added some basic scroll to element function

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,8 @@ import {Grid} from '@material-ui/core';
 import Background from './img/hedonism.jpg';
 import {Typography} from '@material-ui/core';
 import { Button } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
+
 
 const sectionStyle = {
   height: "100vh",
@@ -15,33 +17,49 @@ const sectionStyle = {
   backgroundPosition: 'center',
 };
 
+
+
+
 const Header = () => {
+  const bottomRef = React.useRef();
+
+  const onClick = () => {
+    bottomRef.current.scrollIntoView({behavior: 'smooth'});
+  }
   return (
+    <div>
     <Grid style={sectionStyle}
         container
         direction="column"
-        justify="space-evenly"
+        justify="center"
+        alignContent="center"
         alignItems="center"
         margin="50px"
     >
       <Grid item>
-        <Typography variant="h1">
-          Find the best restaurants in your area
+        <Typography variant="h2"
+        textAlign="center"
+        fontWeight="fontWeightBold">
+          <Box fontWeight={500} m={1}>
+          ALL THE BEST RESTAURANTS IN ONE PLACE
+          </Box>
         </Typography>
         <Button 
-        
-        style={{
-      
-          borderColor: '#fff',
-          color: "#fff",
-          padding: "18px 36px",
-          fontSize: "18px",
-          marginTop: '50px'}}
-        variant="outlined" >
-          Discover
+            onClick={onClick}
+            style={{
+              borderColor: '#fff',
+              color: "#fff",
+              padding: "18px 36px",
+              fontSize: "18px",
+              marginTop: '50px'}}
+            variant="outlined" >
+              Discover
         </Button>
       </Grid>
     </Grid>
+     <div className="bottomContainerElement" ref={bottomRef}> 
+     </div>
+      </div>
   );
 };
 export default Header;
